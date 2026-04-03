65b806c808f741f403447e94ff88bc8dac51e854
dbarcene <david.barcene@utp.ac.pa>
2026-04-03 18:32:56 -0500

    char and line counting, for, while, ++nc

--- 03_chario/for_char_counting.c
+++ 03_chario/for_char_counting.c
@@ -1,22 +1,19 @@
 /**
- * File              : line_counting.c
+ * File              : for_char_counting.c
  * Author            : David Barcene <dbarcene@indicasat.org.pa>
  * Date              : 02.04.2026
  * Last Modified Date: 03.04.2026
  * Last Modified By  : David Barcene <dbarcene@indicasat.org.pa>
  */
+
 #include <stdio.h>
 
 int main() {
 
 	printf("Write some characters then press ENTER and Ctrl+D\n");
 
-	int c, nl;
-
-	nl = 0;
-	while ((c = getchar()) != EOF) {
-		if (c == '\n')
-			++nl;
-	}
-	printf("%d", nl);
+	double nc;
+	for (nc = 0; getchar() != EOF; ++nc)
+		;
+	printf("%.0f\n", nc);
 }
