88b5cbea51b9b8b8e2d35277d4852b9553067d02
dbarcene <david.barcene@utp.ac.pa>
2026-04-03 21:57:02 -0500

    excercise 1,8 of K&R C

---
+++ 03_chario/exercise-1.8.c
@@ -0,0 +1,33 @@
+/**
+ * File              : exercise-1.8.c
+ * Author            : David Barcene <dbarcene@indicasat.org.pa>
+ * Date              : 03.04.2026
+ * Last Modified Date: 03.04.2026
+ * Last Modified By  : David Barcene <dbarcene@indicasat.org.pa>
+ */
+#include <stdio.h>
+
+int main() {
+	printf("This program counts blanks, tabs, and newlines.\n"
+	       "Write something:\n");
+
+	int newline, tabs, spaces;
+
+	int chars;
+
+	newline = 0;
+	tabs = 0;
+	spaces = 0;
+	while ((chars = getchar()) != EOF) {
+		if (chars == '\n')
+			++newline;
+		else if (chars == '\t')
+			++tabs;
+		else if (chars == ' ')
+			++spaces;
+	}
+
+	printf("newline= %d\n", newline);
+	printf("tabs= %d\n", tabs);
+	printf("spaces= %d\n", spaces);
+}
